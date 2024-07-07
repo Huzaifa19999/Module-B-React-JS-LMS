64ae12244997989aa309e202e535e4a7e9c72c5c
Huzaifa19999 <huzaifabhai10@gmail.com>
2024-07-08 02:42:12 +0500

    LMS

--- src/screens/Exam/examresult.tsx
+++ src/screens/Exam/examresult.tsx
@@ -3,38 +3,61 @@ import { getData } from "../../config/firebaseMethods"
 
 function Examresult() {
 
-  const [  setStudents] = useState<any>([])
-  // const [ subject1, setSubject1 ] = useState<any>([])
-  // const [ subject2, setSubject2 ] = useState<any>([])
-  // const [ subject3, setSubject3 ] = useState<any>([])
-  // const [ subject4, setSubject4 ] = useState<any>([])
+  const [ exam , setExam ] = useState<any>([])
+
+
+
 
   useEffect(()=>{
-    getData('Student Data')
-    .then((res)=>{
-      console.log(res)
-      setStudents(...[res])
-    }).catch((err)=>{
+    getData('Exam Data')
+    .then((res:any)=>{
+      console.log(...[Object.values(res)])
+      setExam(...[Object.values(res)])
+    }).catch((err:any)=>{
       console.log(err)
     })
   },[])
-
+  
   return (
     <>
-        {/* <h3 className="fw-bold">Enter Student Name</h3>
-        <select className="form-control">
-          <option value="">Select Student</option>
-          {students.map((e:any,i:any)=>(
-            <option key={i}>{e.name}</option>
-          ))}
-        </select> */}
-
-    
-    {/* <LMS_Input placeholder="Enter 1st Subject" label={""} onChange={function (e: any): void {
-        throw new Error("Function not implemented.")
-      } } value={student} className="form control" /> */}
+            <table className=" table table-bordered">
+              <thead className="">
+              <tr>
+              <th>Name</th>
+              <th>Teacher Name</th>
+              <th>Class</th>
+              <th>Subject No 1</th>
+              <th>Subject No 2</th>
+              <th>Subject No 3</th>
+              <th>Subject No 4</th>
+              <th>Marks in 1st Subject</th>
+              <th>Marks in 2nd Subject</th>
+              <th>Marks in 3rd Subject</th>
+              <th>Marks in 4th Subject</th>
+              <th>Total Marks</th>
+              </tr>
+              </thead>
+              <tbody>
+             {exam.map((e:any,i:any)=>(
+              <tr key={i}>
+                <td>{e.student}</td>
+                <td>{e.teacher}</td>
+                <td>{e.Class}</td>
+                <td>{e.subject1}</td>
+                <td>{e.subject2}</td>
+                <td>{e.subject3}</td>
+                <td>{e.subject4}</td>
+                <td>{e.mark1}</td>
+                <td>{e.mark2}</td>
+                <td>{e.mark3}</td>
+                <td>{e.mark4}</td>
+                <td></td>
+              </tr>
+            ))}
+            </tbody>
+             </table>
     </>
   )
-}
 
+}
 export default Examresult
